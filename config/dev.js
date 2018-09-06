41f048fc9fa7aa51b8b6722be6a688f06162617f
unknown <saintnicks78@hotmail.com>
2018-09-06 14:13:42 -0400

    passport/login

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,7 @@
 module.exports = {
-
-	mongoURI:'mongodb://saintnicks78:cutekitteN99@ds145752.mlab.com:45752/s3uploads'
+    googleClientID:'686026913833-t96p3u9njrt9cvrvmc1578rvb6h1traq.apps.googleusercontent.com',
+    googleClientSecret:'xa5yfdED0pmZghr3N-Tch9hl',
+	mongoURI:'mongodb://saintnicks78:cutekitteN99@ds145752.mlab.com:45752/s3uploads',
+	cookieKey: 'alpha123'
 
 }
--- index.js
+++ index.js
@@ -2,13 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose - require('mongoose');
 const keys = require('./config/keys');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+
+
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, { useMongoClient: true });
 
 const app = express();
-app.use(bodyParser.json());
 
+
+app.use(bodyParser.json());
+app.use(cookieSession({
+
+	maxAge:30 * 24 * 60 * 60 * 1000,
+	keys:[keys.cookieKey]
+
+
+   })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 const PORT = process.env.PORT || 5000;
